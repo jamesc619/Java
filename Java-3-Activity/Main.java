404f92bdbba773e02a2a7196157f0a95af5d2c57
jamesc619 <jamesc619@nycstudents.net>
2025-09-12 16:16:31 +0000

    hi

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Hi");
+
+   System.out.println("James Choy");
+
+   System.out.println("123 Very Real Street");
+
+   System.out.println("James Choy, 123 Very Real Street");
+    
+  }
+
+  
+}
